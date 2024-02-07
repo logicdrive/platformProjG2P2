d9e23f88f40562c997906b9133c4484e935ec695
logicdrive <logicdrive7@gmail.com>
2024-02-07 23:43:17 +0900

    Feat: Form UI 스타일 조정

--- externalTool/serverTester/ServerTester/ServerTester/MainForm.Designer.cs
+++ externalTool/serverTester/ServerTester/ServerTester/MainForm.Designer.cs
@@ -217,7 +217,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TestGroupListBox);
             this.Controls.Add(this.TestExecuteButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ServerTester";
