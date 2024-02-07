aed7dd043ee79da51bb8229aec964bdfc6894220
logicdrive <logicdrive7@gmail.com>
2024-02-07 11:54:33 +0900

    Refactor: 로깅 부분을 상위 클래스 global로 분리

--- source/user/src/main/java/bookGenerator/domain/User.java
+++ source/user/src/main/java/bookGenerator/domain/User.java
@@ -6,30 +6,27 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import bookGenerator._global.logger.CustomLogger;
-import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator._global.infra.LoggedEntity;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.PostPersist;
-import javax.persistence.PostRemove;
-import javax.persistence.PostUpdate;
 import javax.persistence.PrePersist;
-import javax.persistence.PreRemove;
 import javax.persistence.PreUpdate;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "App_User")
-public class User {    
+public class User extends LoggedEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -52,60 +49,13 @@ public class User {
         this.createdDate = new Date();
         this.updatedDate = new Date();
 
-
-        CustomLogger.debugObject(
-            CustomLoggerType.EFFECT,
-            String.format("Try to create %s by using JPA", this.getClass().getSimpleName()),
-            this
-        );
+        super.onPrePersist();
     }
-
-    @PostPersist
-    public void onPostPersist() {
-        CustomLogger.debugObject(
-            CustomLoggerType.EFFECT,
-            String.format("%s is created by using JPA", this.getClass().getSimpleName()),
-            this
-        );
-    }
-
 
     @PreUpdate
     public void onPreUpdate() {
         this.updatedDate = new Date();
-        
-        CustomLogger.debugObject(
-            CustomLoggerType.EFFECT,
-            String.format("Try to update %s by using JPA", this.getClass().getSimpleName()),
-            this
-        );
-    }
 
-    @PostUpdate
-    public void onPostUpdate() {
-        CustomLogger.debugObject(
-            CustomLoggerType.EFFECT,
-            String.format("%s is updated by using JPA", this.getClass().getSimpleName()),
-            this
-        );
-    }
-
-
-    @PreRemove
-    public void onPreRemove() {
-        CustomLogger.debugObject(
-            CustomLoggerType.EFFECT,
-            String.format("Try to delete %s by using JPA", this.getClass().getSimpleName()),
-            this
-        );
-    }
-
-    @PostRemove
-    public void onPostRemove() {
-        CustomLogger.debugObject(
-            CustomLoggerType.EFFECT,
-            String.format("%s is deleted by using JPA", this.getClass().getSimpleName()),
-            this
-        );
+        super.onPreUpdate();
     }
 }
