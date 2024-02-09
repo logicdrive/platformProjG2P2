d841aeecd67b52b4f8fa6716a7beba4f4192c2c8
logicdrive <logicdrive7@gmail.com>
2024-02-09 12:58:12 +0900

    Feat: 커멘드 변수가 자동으로 세팅되도록 만듬

--- externalTool/baseCodeGenerator/index.js
+++ externalTool/baseCodeGenerator/index.js
@@ -16,12 +16,16 @@ function main() {
     fs.renameSync('./output/base/src/main/java/SERVICE_INFO_PACKAGE_NAME',  
               `./output/base/src/main/java/${settings.SERVICE_INFO.PACKAGE_NAME}`)
     const options = {
-        files: ['./output/base/src/main/**/*.*',
-                './output/base/pom.xml'],
+        files: ['./output/base/src/main/**/*.*', './output/base/pom.xml', 
+                './output/base/command/docker/value/**/*.*', './output/base/command/kubernetes/value/**/*.*'],
         from: [/\[\[SERVICE_INFO\.PACKAGE_NAME\]\]/g, /\[\[SERVICE_INFO\.SERVICE_NAME\]\]/g, 
-            /\[\[SERVICE_INFO\.OPEN_PORT\]\]/g, /\[\[SERVICE_INFO\.HOST_PORT\]\]/g],
+            /\[\[SERVICE_INFO\.OPEN_PORT\]\]/g, /\[\[SERVICE_INFO\.HOST_PORT\]\]/g,
+            /\[\[SERVICE_INFO\.DOCKER_IMAGE_NAME\]\]/g, /\[\[SERVICE_INFO\.DOCKER_SERVICE_NAME\]\]/g,
+            /\[\[SERVICE_INFO\.DOKCER_IMAGE_UESR_NAME\]\]/g],
         to: [settings.SERVICE_INFO.PACKAGE_NAME, settings.SERVICE_INFO.SERVICE_NAME, 
-            settings.SERVICE_INFO.OPEN_PORT, settings.SERVICE_INFO.HOST_PORT]
+            settings.SERVICE_INFO.OPEN_PORT, settings.SERVICE_INFO.HOST_PORT,
+            settings.SERVICE_INFO.DOCKER_IMAGE_NAME, settings.SERVICE_INFO.DOCKER_SERVICE_NAME,
+            settings.SERVICE_INFO.DOKCER_IMAGE_UESR_NAME]
     }
     replace.sync(options)
 }
