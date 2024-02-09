f91da335f93fcc0208e5a902559e3bcfd11c22b7
logicdrive <logicdrive7@gmail.com>
2024-02-09 13:15:46 +0900

    Refactor: 코드를 모듈 방식으로 불러오도록 만듬

--- externalTool/baseCodeGenerator/index.js
+++ externalTool/baseCodeGenerator/index.js
@@ -1,20 +1,16 @@
+import fs from 'fs'
+import replace from 'replace-in-file'
+
+
 function main() {
-    // Check ./output/base directory exists. If exist, remove recursively.
-    const fs = require('fs')
-    if (fs.existsSync('./output/base')) {
-        fs.rmSync('./output/base', { recursive: true })
-    }
-    
-    // Copy all directory and files in ./template/base to ./output/base
-    fs.cpSync('./template/base', './output/base', { recursive: true })
+    const settings = readJsonFile('./input/settings.json')
 
 
-    // Replace all instances of 'hello' with 'world' in ./output/base
-    const replace = require('replace-in-file')
-    const settings = require('./input/settings.json')
-
+    copyAllRecursive('./template/base', './output/base')
     fs.renameSync('./output/base/src/main/java/SERVICE_INFO_PACKAGE_NAME',  
               `./output/base/src/main/java/${settings.SERVICE_INFO.PACKAGE_NAME}`)
+
+    
     const options = {
         files: ['./output/base/src/main/**/*.*', './output/base/pom.xml', 
                 './output/base/command/docker/value/**/*.*', './output/base/command/kubernetes/value/**/*.*'],
@@ -28,6 +24,19 @@ function main() {
             settings.SERVICE_INFO.DOKCER_IMAGE_UESR_NAME]
     }
     replace.sync(options)
+}
+
+
+function readJsonFile(path) {
+    return JSON.parse(fs.readFileSync(path, 'utf8'))
+}
+
+function copyAllRecursive(srcPath, destPath) {    
+    if (fs.existsSync(destPath)) {
+        fs.rmSync(destPath, { recursive: true })
+    }
+    
+    fs.cpSync(srcPath, destPath, { recursive: true })
 }
 
 main()
