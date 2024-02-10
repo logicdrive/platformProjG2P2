b9fe46e9310437cd7b61fc18cb23390d838fd5cc
logicdrive <logicdrive7@gmail.com>
2024-02-10 18:28:07 +0900

    Feat: createTag 엔드포인트 설계

--- source/tag/src/main/java/bookGenerator/endPoint/CreateTagEndPoints.java
+++ source/tag/src/main/java/bookGenerator/endPoint/CreateTagEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,10 +9,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Tag;
+
+
+@Data
+@ToString
+class CreateTagReqDto {
+    private final Long bookId;
+    private final String name;
+}
+
+@Getter
+@ToString
+class CreateTagResDto {
+    private final Long id;
+
+    public CreateTagResDto(Tag tag) {
+        this.id = tag.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,10 +41,17 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class CreateTagEndPoints {
 
     @PutMapping("/createTag")
-    public ResponseEntity<Void> createTag() {
+    public ResponseEntity<Void> createTag(CreateTagReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debug(CustomLoggerType.ENTER, reqDto.toString());
+
+            // [1] 새로운 Tag 객체를 생성
+            // [!] bookId, name만 초기화시키면 되며, 다른 변수들은 자동으로 초기화됨
+
+            // [2] TagCreated 이벤트를 발생시킴
+
+            // [3] 생성된 Tag 객체의 id를 반환함
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
