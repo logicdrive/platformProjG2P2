b034ae50662f82b2eb419c3419b4c024db2bda0a
logicdrive <logicdrive7@gmail.com>
2024-02-10 15:08:06 +0900

    Feat: DeleteBook 엔드포인트 설계 추가

--- source/book/src/main/java/bookGenerator/endPoint/DeleteBookEndPoints.java
+++ source/book/src/main/java/bookGenerator/endPoint/DeleteBookEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,10 +9,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Book;
+
+
+@Data
+@ToString
+class DeleteBookReqDto {
+    private final Long bookId;
+}
+
+@Getter
+@ToString
+class DeleteBookResDto {
+    private final Long id;
+
+    public DeleteBookResDto(Book book) {
+        this.id = book.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,11 +40,19 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class DeleteBookEndPoints {
 
     @PutMapping("/deleteBook")
-    public ResponseEntity<Void> deleteBook() {
+    public ResponseEntity<Void> deleteBook(DeleteBookReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
-                
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+            
+            // [1] reqDto.bookId로 Book 객체를 찾음
+
+            // [2] Book 객체를 삭제
+
+            // [3] BookDeleted 이벤트를 찾은 Book 객체로 발생시킴
+            
+            // [4] 찾은 Book 객체의 ID를 반환
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
