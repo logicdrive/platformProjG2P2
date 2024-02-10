0fd1978d9c27c81abd816e8ff283bd480845344d
logicdrive <logicdrive7@gmail.com>
2024-02-11 08:37:39 +0900

    Feat: generateContent 엔드포인트 설계

--- source/content/src/main/java/bookGenerator/_global/event/ContentImageGenerationRequested.java
+++ source/content/src/main/java/bookGenerator/_global/event/ContentImageGenerationRequested.java
@@ -3,13 +3,15 @@ package bookGenerator._global.event;
 import lombok.NoArgsConstructor;
 
 import bookGenerator._global.eventBase.EventNameAnnotation;
-import bookGenerator._global.eventBase.ContentEvent;
-import bookGenerator.domain.Content;
+import bookGenerator._global.infra.AbstractEvent;
 
 @EventNameAnnotation(eventName="ContentImageGenerationRequested")
 @NoArgsConstructor
-public class ContentImageGenerationRequested extends ContentEvent {
-    public ContentImageGenerationRequested(Content aggregate) {
-        super(aggregate);
+public class ContentImageGenerationRequested extends AbstractEvent {
+    private Long indexId;;
+
+    public ContentImageGenerationRequested(Long indexId) {
+        super();
+        this.indexId = indexId;
     }
 }
--- source/content/src/main/java/bookGenerator/endPoint/GenerateContentEndPoints.java
+++ source/content/src/main/java/bookGenerator/endPoint/GenerateContentEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,16 +15,26 @@ import lombok.ToString;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 
+
+@Data
+@ToString
+class GenerateContentReqDto {
+    private final Long indexId;
+}
+
+
 @RestController
 @Transactional
 @RequestMapping("/contents")
 public class GenerateContentEndPoints {
 
     @PutMapping("/generateContent")
-    public ResponseEntity<Void> generateContent() {
+    public ResponseEntity<Void> generateContent(@RequestBody GenerateContentReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debug(CustomLoggerType.ENTER, reqDto.toString());
+
+            // [1] ContentImageGenerationRequested 이벤트를 indexId를 기반으로 생성함
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
