e6004a377b608b8f1e007efbd11084ef1d6d46d8
logicdrive <logicdrive7@gmail.com>
2024-02-10 19:01:47 +0900

    Feat: bookDeleted_deleteTag_Policy 정책 설계

--- source/tag/src/main/java/bookGenerator/domain/TagRepository.java
+++ source/tag/src/main/java/bookGenerator/domain/TagRepository.java
@@ -1,9 +1,12 @@
 package bookGenerator.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "tags", path = "tags")
 public interface TagRepository
     extends PagingAndSortingRepository<Tag, Long> {
+    List<Tag> findByBookId(Long bookId);
 }
--- source/tag/src/main/java/bookGenerator/policy/BookDeleted_deleteTag_Policy.java
+++ source/tag/src/main/java/bookGenerator/policy/BookDeleted_deleteTag_Policy.java
@@ -15,7 +15,7 @@ import bookGenerator._global.event.BookDeleted;
 @Transactional
 public class BookDeleted_deleteTag_Policy {
 
-    // BookDeleted 이벤트 발생 관련 정책
+    // 책이 삭제되었을 경우, 부여된 태그들을 삭제하기 위한 정책
     @StreamListener(
         value = KafkaProcessor.INPUT,
         condition = "headers['type']=='BookDeleted'"
@@ -25,8 +25,13 @@ public class BookDeleted_deleteTag_Policy {
     ) {
         try
         {
-            
-            CustomLogger.debugObject(CustomLoggerType.ENTER_EXIT, "BookDeleted", bookDeleted);
+            CustomLogger.debugObject(CustomLoggerType.ENTER, bookDeleted);
+
+            // [1] 해당 책에 부여된 태그들을 삭제
+
+            // [2] 각각의 삭제된 태그들에 대해서 TagDeleted 이벤트를 발생시킴
+
+            CustomLogger.debug(CustomLoggerType.EXIT);
 
         } catch(Exception e) {
             CustomLogger.errorObject(e, "", bookDeleted);        
