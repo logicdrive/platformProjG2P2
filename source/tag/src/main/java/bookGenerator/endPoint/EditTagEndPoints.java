408739a7fae5429634a57c6f3ef3974ec39b34cc
logicdrive <logicdrive7@gmail.com>
2024-02-10 18:38:09 +0900

    Feat: editTag 엔드포인트 설계

--- source/tag/src/main/java/bookGenerator/endPoint/EditTagEndPoints.java
+++ source/tag/src/main/java/bookGenerator/endPoint/EditTagEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,10 +9,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Tag;
+
+
+@Data
+@ToString
+class EditTagReqDto {
+    private final Long tagId;
+    private final String tagName;
+}
+
+@Getter
+@ToString
+class EditTagResDto {
+    private final Long id;
+
+    public EditTagResDto(Tag tag) {
+        this.id = tag.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,10 +41,18 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class EditTagEndPoints {
 
     @PutMapping("/editTag")
-    public ResponseEntity<Void> editTag() {
+    public ResponseEntity<Void> editTag(EditTagReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debug(CustomLoggerType.ENTER, reqDto.toString());
+
+            // [1] tagId에 해당하는 Tag 객체를 찾음
+
+            // [2] Tag 객체의 name을 변경함
+
+            // [3] 저장된 Tag를 기반으로 TagEdited 이벤트를 발생시킴
+
+            // [4] 저장된 Tag Id를 반환함
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
