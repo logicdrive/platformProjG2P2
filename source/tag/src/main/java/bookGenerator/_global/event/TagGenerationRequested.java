8c09c01b9a421afb9d4e5b876d2905dafdd29f56
logicdrive <logicdrive7@gmail.com>
2024-02-10 16:27:56 +0900

    Feat: generateTags 엔드포인트 설계

--- source/tag/src/main/java/bookGenerator/_global/event/TagGenerationRequested.java
+++ source/tag/src/main/java/bookGenerator/_global/event/TagGenerationRequested.java
@@ -3,13 +3,15 @@ package bookGenerator._global.event;
 import lombok.NoArgsConstructor;
 
 import bookGenerator._global.eventBase.EventNameAnnotation;
-import bookGenerator._global.eventBase.TagEvent;
-import bookGenerator.domain.Tag;
+import bookGenerator._global.infra.AbstractEvent;
 
 @EventNameAnnotation(eventName="TagGenerationRequested")
 @NoArgsConstructor
-public class TagGenerationRequested extends TagEvent {
-    public TagGenerationRequested(Tag aggregate) {
-        super(aggregate);
+public class TagGenerationRequested extends AbstractEvent {
+    Long bookId;
+
+    public TagGenerationRequested(Long bookId) {
+        super();
+        this.bookId = bookId;
     }
 }
--- source/tag/src/main/java/bookGenerator/endPoint/GenerateTagsEndPoints.java
+++ source/tag/src/main/java/bookGenerator/endPoint/GenerateTagsEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,17 +14,27 @@ import lombok.ToString;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 
+
+@Data
+@ToString
+class GenerateTagsReqDto {
+    private final Long bookId;
+}
+
+
 @RestController
 @Transactional
 @RequestMapping("/tags")
 public class GenerateTagsEndPoints {
 
     @PutMapping("/generateTags")
-    public ResponseEntity<Void> generateTags() {
+    public ResponseEntity<Void> generateTags(GenerateTagsReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
-                
+            CustomLogger.debug(CustomLoggerType.ENTER, reqDto.toString());
+
+            // [1] TagGenerationRequested 이벤트를 bookId를 기반으로 생성함
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
