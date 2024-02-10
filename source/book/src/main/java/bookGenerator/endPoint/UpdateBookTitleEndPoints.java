322e77bd0577c834915eaf5884f4989b71acd8d3
logicdrive <logicdrive7@gmail.com>
2024-02-10 14:29:22 +0900

    Feat: updateBookTitle 엔드포인트 설계 추가

--- source/book/src/main/java/bookGenerator/endPoint/UpdateBookTitleEndPoints.java
+++ source/book/src/main/java/bookGenerator/endPoint/UpdateBookTitleEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,10 +9,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Book;
+
+
+@Data
+@ToString
+class UpdateBookTitleReqDto {
+    private final Long bookId;
+    private final String bookTitle;
+}
+
+@Getter
+@ToString
+class UpdateBookTitleResDto {
+    private final Long id;
+
+    public UpdateBookTitleResDto(Book book) {
+        this.id = book.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,10 +41,18 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class UpdateBookTitleEndPoints {
 
     @PutMapping("/updateBookTitle")
-    public ResponseEntity<Void> updateBookTitle() {
+    public ResponseEntity<Void> updateBookTitle(UpdateBookTitleReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+
+            // [1] reqDto.bookId로 Book 객체를 찾음
+
+            // [2] reqDto.bookTitle로 Book 객체의 제목을 변경하고 저장함
+
+            // [3] BookTitleUpdated 이벤트를 저장한 Book 객체로 발생시킴
+
+            // [4] 저장한 Book 객체의 ID를 반환
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
