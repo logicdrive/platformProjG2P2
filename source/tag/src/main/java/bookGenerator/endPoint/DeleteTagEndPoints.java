45e0a24fd5df37f6f8a39d03113aa9f1eb4af4d6
logicdrive <logicdrive7@gmail.com>
2024-02-10 18:43:48 +0900

    Feat: DeleteTag 엔드포인트 설계

--- source/tag/src/main/java/bookGenerator/endPoint/DeleteTagEndPoints.java
+++ source/tag/src/main/java/bookGenerator/endPoint/DeleteTagEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,10 +9,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Tag;
+
+
+@Data
+@ToString
+class DeleteTagReqDto {
+    private final Long tagId;
+}
+
+@Getter
+@ToString
+class DeleteTagResDto {
+    private final Long id;
+
+    public DeleteTagResDto(Tag tag) {
+        this.id = tag.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,11 +40,19 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class DeleteTagEndPoints {
 
     @PutMapping("/deleteTag")
-    public ResponseEntity<Void> deleteTag() {
+    public ResponseEntity<Void> deleteTag(DeleteTagReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
                 
+            // [1] reqDto.tagId로 Tag 객체를 찾음
+
+            // [2] Tag 객체를 삭제
+
+            // [3] TagDeleted 이벤트를 찾은 Tag 객체로 발생시킴
+            
+            // [4] 찾은 Tag 객체의 ID를 반환
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
