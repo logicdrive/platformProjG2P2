1304cde06e55dd05fb654442cffa4ca7af30412e
logicdrive <logicdrive7@gmail.com>
2024-02-10 20:47:07 +0900

    Feat: deleteIndex 엔드포인트 설계

--- source/index/src/main/java/bookGenerator/endPoint/DeleteIndexEndPoints.java
+++ source/index/src/main/java/bookGenerator/endPoint/DeleteIndexEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Index;
+
+
+@Data
+@ToString
+class DeleteIndexReqDto {
+    private final Long indexId;
+}
+
+@Getter
+@ToString
+class DeleteIndexResDto {
+    private final Long id;
+
+    public DeleteIndexResDto(Index index) {
+        this.id = index.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,10 +41,18 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class DeleteIndexEndPoints {
 
     @PutMapping("/deleteIndex")
-    public ResponseEntity<Void> deleteIndex() {
+    public ResponseEntity<Void> deleteIndex(@RequestBody DeleteIndexReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debug(CustomLoggerType.ENTER, reqDto.toString());
+
+            // [1] reqDto.indexId로 Index 객체를 찾음
+
+            // [2] Index 객체를 삭제
+
+            // [3] IndexDeleted 이벤트를 찾은 Index 객체로 발생시킴
+            
+            // [4] 찾은 Index 객체의 ID를 반환
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
