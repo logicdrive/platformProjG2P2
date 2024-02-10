ca7eb39a3afc8d832c015ff0a4f8f03f2f2311e6
logicdrive <logicdrive7@gmail.com>
2024-02-10 14:17:59 +0900

    Feat: updateCoverImage 엔드포인트 설계 추가

--- source/book/src/main/java/bookGenerator/_global/event/CoverImageUpdateRequested.java
+++ source/book/src/main/java/bookGenerator/_global/event/CoverImageUpdateRequested.java
@@ -9,7 +9,15 @@ import bookGenerator.domain.Book;
 @EventNameAnnotation(eventName="CoverImageUpdateRequested")
 @NoArgsConstructor
 public class CoverImageUpdateRequested extends BookEvent {
-    public CoverImageUpdateRequested(Book aggregate) {
+    String imageUrl;
+
+    public CoverImageUpdateRequested(Book aggregate, String imageUrl) {
         super(aggregate);
+        this.imageUrl = imageUrl;
+    }
+
+    public String toString() {
+        return String.format("%s(bookId=%d, imageUrlLength=%d)",
+            this.getClass().getSimpleName(), this.id, this.imageUrl.length());
     }
 }
--- source/book/src/main/java/bookGenerator/endPoint/UpdateCoverImageEndPoints.java
+++ source/book/src/main/java/bookGenerator/endPoint/UpdateCoverImageEndPoints.java
@@ -1,9 +1,7 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,22 +9,52 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Book;
 
 @RestController
 @Transactional
 @RequestMapping("/books")
 public class UpdateCoverImageEndPoints {
 
+    @Data
+    class UpdateCoverImageeReqDto {
+        private final Long bookId;
+        private final String imageUrl;
+        
+        public String toString() { 
+            return String.format("%s(bookId=%s, imageUrlLength=%d)",
+                this.getClass().getSimpleName(), this.bookId, this.imageUrl.length());
+        }
+    }
+
+    @Getter
+    @ToString
+    class UpdateCoverImageResDto {
+        private final Long id;
+
+        public UpdateCoverImageResDto(Book book) {
+            this.id = book.getId();
+        }
+    }
+
+
     @PutMapping("/updateCoverImage")
-    public ResponseEntity<Void> updateCoverImage() {
+    public ResponseEntity<Void> updateCoverImage(UpdateCoverImageeReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
-                
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+
+            // [1] reqDto.bookId로 Book 객체를 찾음
+
+            // [2] CoverImageUpdateRequested 이벤트를 찾은 Book 객체 및 DataUrl로 발생시킴
+
+            // [3] 찾은 Book 객체의 ID를 반환
+            
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
