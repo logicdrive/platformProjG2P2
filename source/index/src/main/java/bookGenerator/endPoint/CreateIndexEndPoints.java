dce4d85a0fe16479d9e046a8a94e2d570400e136
logicdrive <logicdrive7@gmail.com>
2024-02-10 20:14:36 +0900

    Feat: createIndex 엔드포인트 설계

--- source/index/src/main/java/bookGenerator/endPoint/CreateIndexEndPoints.java
+++ source/index/src/main/java/bookGenerator/endPoint/CreateIndexEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Index;
+
+
+@Data
+@ToString
+class CreateIndexReqDto {
+    private final Long bookId;
+    private final String name;
+    private final Long priority;
+}
+
+@Getter
+@ToString
+class CreateIndexResDto {
+    private final Long id;
+
+    public CreateIndexResDto(Index index) {
+        this.id = index.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,11 +43,18 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class CreateIndexEndPoints {
 
     @PutMapping("/createIndex")
-    public ResponseEntity<Void> createIndex() {
+    public ResponseEntity<Void> createIndex(@RequestBody CreateIndexReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
-                
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+            
+            // [1] 새로운 Index 객체를 생성
+            // [!] bookId, name, priority만 초기화시키면 되며, 다른 변수들은 자동으로 초기화됨
+
+            // [2] IndexCreated 이벤트를 발생시킴
+
+            // [3] 생성된 Index 객체의 id를 반환함
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
