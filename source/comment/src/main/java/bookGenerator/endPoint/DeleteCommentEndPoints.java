28dbeb844fc4b349cca386ba7e87683c40008f49
logicdrive <logicdrive7@gmail.com>
2024-02-11 13:25:51 +0900

    Feat: updateComment, deleteComment 설계

--- source/comment/src/main/java/bookGenerator/endPoint/DeleteCommentEndPoints.java
+++ source/comment/src/main/java/bookGenerator/endPoint/DeleteCommentEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Comment;
+
+
+@Data
+@ToString
+class DeleteCommentReqDto {
+    private final Long commentId;
+}
+
+@Getter
+@ToString
+class DeleteCommentResDto {
+    private final Long id;
+
+    public DeleteCommentResDto(Comment comment) {
+        this.id = comment.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,10 +41,18 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class DeleteCommentEndPoints {
 
     @PutMapping("/deleteComment")
-    public ResponseEntity<Void> deleteComment() {
+    public ResponseEntity<Void> deleteComment(@RequestBody DeleteCommentResDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+
+            // [1] reqDto.commentId로 Comment 객체를 찾음
+
+            // [2] Comment 객체를 삭제
+
+            // [3] CommentDeleted 이벤트를 찾은 Comment 객체로 발생시킴
+            
+            // [4] 찾은 Comment 객체의 ID를 반환
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
--- source/comment/src/main/java/bookGenerator/endPoint/UpdateCommentEndPoints.java
+++ source/comment/src/main/java/bookGenerator/endPoint/UpdateCommentEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.Comment;
+
+
+@Data
+@ToString
+class UpdateCommentReqDto {
+    private final Long commentId;
+    private final String content;
+}
+
+@Getter
+@ToString
+class UpdateCommentResDto {
+    private final Long id;
+
+    public UpdateCommentResDto(Comment comment) {
+        this.id = comment.getId();
+    }
+}
+
+
 
 @RestController
 @Transactional
@@ -22,11 +43,19 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class UpdateCommentEndPoints {
 
     @PutMapping("/updateComment")
-    public ResponseEntity<Void> updateComment() {
+    public ResponseEntity<Void> updateComment(@RequestBody UpdateCommentReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
-                
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+          
+            // [1] reqDto.commentId로 Comment 객체를 찾음
+
+            // [2] reqDto.content로 Comment 객체의 내용을 변경하고 저장함
+
+            // [3] CommentUpdated 이벤트를 저장한 Comment 객체로 발생시킴
+
+            // [4] 저장한 Comment 객체의 ID를 반환
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
