acefb75c91e6b20c5f982c71c02b299f965807ce
logicdrive <logicdrive7@gmail.com>
2024-02-11 18:38:01 +0900

    Feat: addBookShelfBook, deleteBookShelfBook 설계

--- source/bookShelfBook/src/main/java/bookGenerator/endPoint/AddBookShelfBookEndPoints.java
+++ source/bookShelfBook/src/main/java/bookGenerator/endPoint/AddBookShelfBookEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.BookShelfBook;
+
+
+@Data
+@ToString
+class AddBookShelfBookReqDto {
+    private final Long bookShelfId;
+    private final Long bookId;
+}
+
+@Getter
+@ToString
+class AddBookShelfBookResDto {
+    private final Long id;
+
+    public AddBookShelfBookResDto(BookShelfBook bookShelfBook) {
+        this.id = bookShelfBook.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,10 +42,17 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class AddBookShelfBookEndPoints {
 
     @PutMapping("/addBookShelfBook")
-    public ResponseEntity<Void> addBookShelfBook() {
+    public ResponseEntity<Void> addBookShelfBook(@RequestBody AddBookShelfBookReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+
+            // [1] 새로운 BookShelfBook 객체를 생성
+            // [!] bookShelfId, bookId만 초기화시키면 되며, 다른 변수들은 자동으로 초기화됨
+
+            // [2] BookShelfBookAdded 이벤트를 생성된 BookShelfBook로 발생시킴
+
+            // [3] 생성된 BookShelfBook 객체의 ID를 반환
                 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
--- source/bookShelfBook/src/main/java/bookGenerator/endPoint/DeleteBookShelfBookEndPoints.java
+++ source/bookShelfBook/src/main/java/bookGenerator/endPoint/DeleteBookShelfBookEndPoints.java
@@ -1,6 +1,5 @@
 package bookGenerator.endPoint;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import javax.transaction.Transactional;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.BookShelfBook;
+
+
+@Data
+@ToString
+class DeleteBookShelfReqDto {
+    private final Long bookShelfBookId;
+}
+
+@Getter
+@ToString
+class DeleteBookShelfResDto {
+    private final Long id;
+
+    public DeleteBookShelfResDto(BookShelfBook bookShelfBook) {
+        this.id = bookShelfBook.getId();
+    }
+}
+
 
 @RestController
 @Transactional
@@ -22,11 +41,19 @@ import bookGenerator._global.logger.CustomLoggerType;
 public class DeleteBookShelfBookEndPoints {
 
     @PutMapping("/deleteBookShelfBook")
-    public ResponseEntity<Void> deleteBookShelfBook() {
+    public ResponseEntity<Void> deleteBookShelfBook(@RequestBody DeleteBookShelfReqDto reqDto) {
         try {
 
-            CustomLogger.debug(CustomLoggerType.ENTER);
-                
+            CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
+
+            // [1] reqDto.bookShelfBookId로 BookShelfBook 객체를 찾음
+
+            // [2] BookShelfBook 객체를 삭제
+
+            // [3] BookShelfBookDeleted 이벤트를 찾은 BookShelfBook 객체로 발생시킴
+            
+            // [4] 찾은 BookShelfBook 객체의 ID를 반환
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
             return ResponseEntity.status(HttpStatus.OK).build();
