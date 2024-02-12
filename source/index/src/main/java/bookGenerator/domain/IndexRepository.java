ed0dc62657fb36b32131afe62e75cbb93c21a255
logicdrive <logicdrive7@gmail.com>
2024-02-12 17:35:21 +0900

    Fix: 누락된 Repo 함수 추가

--- source/index/src/main/java/bookGenerator/domain/IndexRepository.java
+++ source/index/src/main/java/bookGenerator/domain/IndexRepository.java
@@ -1,9 +1,12 @@
 package bookGenerator.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "indexes", path = "indexes")
 public interface IndexRepository
     extends PagingAndSortingRepository<Index, Long> {
+    List<Index> findByBookId(Long bookId);
 }
