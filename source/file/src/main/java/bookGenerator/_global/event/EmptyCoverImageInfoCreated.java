1e39ed7526b1e45504231c41b62206728f0cdbc0
logicdrive <logicdrive7@gmail.com>
2024-02-13 16:25:45 +0900

    Fix: 이벤트 객체 파라미터 추가

--- source/file/src/main/java/bookGenerator/_global/event/EmptyCoverImageInfoCreated.java
+++ source/file/src/main/java/bookGenerator/_global/event/EmptyCoverImageInfoCreated.java
@@ -1,15 +1,25 @@
 package bookGenerator._global.event;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import bookGenerator._global.eventBase.EventNameAnnotation;
 import bookGenerator._global.eventBase.FileEvent;
+
 import bookGenerator.domain.File;
 
+@Data
 @NoArgsConstructor
+@ToString(callSuper=true)
+@EqualsAndHashCode(callSuper=false)
 @EventNameAnnotation(eventName="EmptyCoverImageInfoCreated")
 public class EmptyCoverImageInfoCreated extends FileEvent {
-    public EmptyCoverImageInfoCreated(File aggregate) {
+    private Long bookId;
+
+    public EmptyCoverImageInfoCreated(File aggregate, Long bookId) {
         super(aggregate);
+        this.bookId = bookId;
     }
 }
