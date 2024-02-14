4c1d2be94cf6ffefc1b7b1a22a681ad538b5e1c4
JWT <ryuhoken30@gamil.com>
2024-02-15 00:47:34 +0900

    EndPoint Fin

--- source/book/src/main/java/bookGenerator/endPoint/UpdateCoverImageEndPoints.java
+++ source/book/src/main/java/bookGenerator/endPoint/UpdateCoverImageEndPoints.java
@@ -12,10 +12,11 @@ import javax.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-
+import bookGenerator._global.event.CoverImageUpdateRequested;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 import bookGenerator.domain.Book;
+import bookGenerator.domain.BookManageService;
 
 
 @Data
@@ -45,20 +46,25 @@ class UpdateCoverImageResDto {
 @RequestMapping("/books")
 public class UpdateCoverImageEndPoints {
     @PutMapping("/updateCoverImage")
-    public ResponseEntity<Void> updateCoverImage(@RequestBody UpdateCoverImageeReqDto reqDto) {
+    public ResponseEntity<Book> updateCoverImage(@RequestBody UpdateCoverImageeReqDto reqDto) {
         try {
 
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
 
             // [1] reqDto.bookId로 Book 객체를 찾음
+            Book bookToUpdateCover = BookManageService.getInstance().findByIdOrThrow(reqDto.getBookId());
 
             // [2] CoverImageUpdateRequested 이벤트를 찾은 Book 객체 및 DataUrl로 발생시킴
+            (new CoverImageUpdateRequested(bookToUpdateCover, reqDto.getImageUrl())).publish();;
 
             // [3] 찾은 Book 객체의 ID를 반환
-            
-            CustomLogger.debug(CustomLoggerType.EXIT);
+            UpdateCoverImageResDto resDto = new UpdateCoverImageResDto(bookToUpdateCover);
 
-            return ResponseEntity.status(HttpStatus.OK).build();
+            CustomLogger.debugObject(CustomLoggerType.EXIT,resDto);
+
+            return ResponseEntity.status(HttpStatus.OK).body(bookToUpdateCover);
+
+            // Fin
 
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
--- source/tag/src/main/java/bookGenerator/endPoint/DeleteTagEndPoints.java
+++ source/tag/src/main/java/bookGenerator/endPoint/DeleteTagEndPoints.java
@@ -12,10 +12,11 @@ import javax.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-
+import bookGenerator._global.event.TagDeleted;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 import bookGenerator.domain.Tag;
+import bookGenerator.domain.TagManageService;
 
 
 @Data
@@ -41,22 +42,28 @@ class DeleteTagResDto {
 public class DeleteTagEndPoints {
 
     @PutMapping("/deleteTag")
-    public ResponseEntity<Void> deleteTag(@RequestBody DeleteTagReqDto reqDto) {
+    public ResponseEntity<Tag> deleteTag(@RequestBody DeleteTagReqDto reqDto) {
         try {
 
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
-                
+
             // [1] reqDto.tagId로 Tag 객체를 찾음
+            Tag tagToDelete = TagManageService.getInstance().findByIdOrThrow(reqDto.getTagId());
 
             // [2] Tag 객체를 삭제
+            Tag.repository().delete(tagToDelete);
 
             // [3] TagDeleted 이벤트를 찾은 Tag 객체로 발생시킴
+            (new TagDeleted(tagToDelete)).publish();
             
             // [4] 찾은 Tag 객체의 ID를 반환
+            DeleteTagResDto resDto = new DeleteTagResDto(tagToDelete);
 
-            CustomLogger.debug(CustomLoggerType.EXIT);
+            CustomLogger.debugObject(CustomLoggerType.EXIT, resDto);
 
-            return ResponseEntity.status(HttpStatus.OK).build();
+            return ResponseEntity.status(HttpStatus.OK).body(tagToDelete);
+
+            // Fin
 
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
