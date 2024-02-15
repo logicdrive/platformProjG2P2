07e7927321665ea3f3e39a2d06b7140ba363bbcb
yj9120 <zzjjxjj1234@gmail.com>
2024-02-15 11:48:34 +0900

    bookShelfBookAdded_increaseBookCount_Policy 정책 구현하기. 점검 부탁드립니다

--- source/bookShelf/src/main/java/bookGenerator/policy/BookShelfBookAdded_IncreaseBookCount_Policy.java
+++ source/bookShelf/src/main/java/bookGenerator/policy/BookShelfBookAdded_IncreaseBookCount_Policy.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import bookGenerator._global.config.kafka.KafkaProcessor;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.BookShelf;
+import bookGenerator.domain.BookShelfManageService;
+import bookGenerator._global.event.BookCountUpdated;
 import bookGenerator._global.event.BookShelfBookAdded;
 
 @Service
@@ -29,10 +32,14 @@ public class BookShelfBookAdded_increaseBookCount_Policy {
             CustomLogger.debugObject(CustomLoggerType.ENTER, bookShelfBookAdded);
 
             // [1] bookShelfId로 BookShelf를 조회함
+            BookShelf bookShelf = BookShelfManageService.getInstance().findByIdOrThrow(bookShelfBookAdded.getId());
+
             
             // [2] 조회된 BookShelf의 bookCount를 1 올림
+            bookShelf.setBookCount(bookShelf.getBookCount() + 1);
             
             // [3] BookCountUpdated 이벤트를 저장시킨 BookShelf로 발생시킴
+            (new BookCountUpdated(bookShelf)).publish();
             
             CustomLogger.debug(CustomLoggerType.EXIT);
 
