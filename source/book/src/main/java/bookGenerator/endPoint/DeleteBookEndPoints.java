9388bfe971a6806b48f9c2cd1625bb6d7b301180
yj9120 <zzjjxjj1234@gmail.com>
2024-02-15 11:20:59 +0900

    deleteBook 엔드포인트 구현하기. 주석 아래로 추가하였습니다. 점검 부탁드립니다!

--- source/book/src/main/java/bookGenerator/endPoint/DeleteBookEndPoints.java
+++ source/book/src/main/java/bookGenerator/endPoint/DeleteBookEndPoints.java
@@ -12,10 +12,11 @@ import javax.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-
+import bookGenerator._global.event.BookDeleted;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 import bookGenerator.domain.Book;
+import bookGenerator.domain.BookManageService;
 
 
 @Data
@@ -47,14 +48,20 @@ public class DeleteBookEndPoints {
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
             
             // [1] reqDto.bookId로 Book 객체를 찾음
+            Book book = BookManageService.getInstance().findByIdOrThrow(reqDto.getBookId());
 
+            
             // [2] Book 객체를 삭제
+            Book.repository().delete(book);
+
 
             // [3] BookDeleted 이벤트를 찾은 Book 객체로 발생시킴
+            (new BookDeleted(book)).publish();
+
             
             // [4] 찾은 Book 객체의 ID를 반환
-
-            CustomLogger.debug(CustomLoggerType.EXIT);
+            DeleteBookResDto deleteBookResDto = new DeleteBookResDto(book);
+            CustomLogger.debugObject(CustomLoggerType.EXIT,deleteBookResDto);
 
             return ResponseEntity.status(HttpStatus.OK).build();
 
