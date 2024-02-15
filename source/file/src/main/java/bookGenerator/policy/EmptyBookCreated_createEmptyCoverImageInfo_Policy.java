ed58755e509f9beb4fde74efed9fcee01451e92f
yj9120 <zzjjxjj1234@gmail.com>
2024-02-15 14:48:07 +0900

    emptyBookCreated_createEmptyCoverImageInfo_Policy 정책 구현하기. 검토 부탁드립니다!

--- source/file/src/main/java/bookGenerator/policy/EmptyBookCreated_createEmptyCoverImageInfo_Policy.java
+++ source/file/src/main/java/bookGenerator/policy/EmptyBookCreated_createEmptyCoverImageInfo_Policy.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 import bookGenerator._global.config.kafka.KafkaProcessor;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.File;
 import bookGenerator._global.event.EmptyBookCreated;
+import bookGenerator._global.event.EmptyCoverImageInfoCreated;
 
 @Service
 @Transactional
@@ -29,10 +31,13 @@ public class EmptyBookCreated_createEmptyCoverImageInfo_Policy {
             CustomLogger.debugObject(CustomLoggerType.ENTER, emptyBookCreated);
 
             // [1] 새로운 File 객체 생성
+            File file = new File();
             // [!] url은 null로 둔다.
             // [!] url만 초기화시키면 되며, 다른 변수들은 자동으로 초기화됨
+            file.setUrl(null);
 
             // [2] EmptyCoverImageInfoCreated 이벤트를 생성된 File 객체로 발생시킨다.
+            (new EmptyCoverImageInfoCreated(file,null)).publish();
 
             CustomLogger.debug(CustomLoggerType.EXIT);
 
