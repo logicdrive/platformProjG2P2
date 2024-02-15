157ddc2a1a2b6a916a0d5c884f86e340ecab28c0
hyunho <shh2849@naver.com>
2024-02-15 23:42:20 +0900

    UpdateBookTitleEndPoints.java 컴토 부탁드립니다.

--- source/book/src/main/java/bookGenerator/endPoint/UpdateBookTitleEndPoints.java
+++ source/book/src/main/java/bookGenerator/endPoint/UpdateBookTitleEndPoints.java
@@ -13,9 +13,12 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
 
+import bookGenerator._global.event.BookTitleUpdated;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+
 import bookGenerator.domain.Book;
+import bookGenerator.domain.BookManageService;
 
 
 @Data
@@ -42,24 +45,29 @@ class UpdateBookTitleResDto {
 public class UpdateBookTitleEndPoints {
 
     @PutMapping("/updateBookTitle")
-    public ResponseEntity<Void> updateBookTitle(@RequestBody UpdateBookTitleReqDto reqDto) {
+    public ResponseEntity<UpdateBookTitleResDto> updateBookTitle(@RequestBody UpdateBookTitleReqDto reqDto) {
         try {
 
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
 
             // [1] reqDto.bookId로 Book 객체를 찾음
+            Book bookToUpdate = BookManageService.getInstance().findByIdOrThrow(reqDto.getBookId());
 
             // [2] reqDto.bookTitle로 Book 객체의 제목을 변경하고 저장함
-
+            bookToUpdate.setTitle(reqDto.getBookTitle());
+            
             // [3] BookTitleUpdated 이벤트를 저장한 Book 객체로 발생시킴
+            Book savedBook = Book.repository().save(bookToUpdate);
+            
+            (new BookTitleUpdated(savedBook)).publish();
 
             // [4] 저장한 Book 객체의 ID를 반환
-                
-            CustomLogger.debug(CustomLoggerType.EXIT);
-
-            return ResponseEntity.status(HttpStatus.OK).build();
+            UpdateBookTitleResDto resDto = new UpdateBookTitleResDto(savedBook);
+            CustomLogger.debugObject(CustomLoggerType.EXIT, "", resDto);
+            return ResponseEntity.ok(resDto);
 
         } catch(Exception e) {
+            CustomLogger.errorObject(e, "", reqDto);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
