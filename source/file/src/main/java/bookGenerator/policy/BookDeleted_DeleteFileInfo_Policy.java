cdc377f2f6edfabcb27f3472076e4c3172af5b33
hyunho <shh2849@naver.com>
2024-02-15 23:59:46 +0900

    bookDeleted_DeleteFileInfo_Policy 정책 구현하기 검토 부탁드립니다.

--- source/file/src/main/java/bookGenerator/policy/BookDeleted_DeleteFileInfo_Policy.java
+++ source/file/src/main/java/bookGenerator/policy/BookDeleted_DeleteFileInfo_Policy.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Service;
 import bookGenerator._global.config.kafka.KafkaProcessor;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.File;
+import bookGenerator.domain.FileManageService;
 import bookGenerator._global.event.BookDeleted;
+import bookGenerator._global.event.FileInfoDeleted;
 
 @Service
 @Transactional
@@ -29,11 +32,14 @@ public class BookDeleted_DeleteFileInfo_Policy {
             CustomLogger.debugObject(CustomLoggerType.ENTER, bookDeleted);
 
             // [1] bookDeleted.coverImageFileId에 해당하는 File의 Id를 조회한다.
-
+            File fileToDelete = FileManageService.getInstance().findByIdOrThrow(bookDeleted.getCoverImageFileId());
+            
             // [2] 조회된 File을 삭제한다.
+            File.repository().delete(fileToDelete);
 
             // [3] 삭제된 File로 FileInfoDeleted 이벤트를 발생시킨다.
-
+            (new FileInfoDeleted(fileToDelete)).publish();
+            
             CustomLogger.debug(CustomLoggerType.EXIT);
 
         } catch(Exception e) {
