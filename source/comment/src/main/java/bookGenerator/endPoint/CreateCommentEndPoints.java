7249c6a5523a4c79abdf5ed05705d7251847aff2
hyunho <shh2849@naver.com>
2024-02-15 23:56:50 +0900

    createComment 엔드포인트 구현하기 검토 부탁드립니다.

--- source/comment/src/main/java/bookGenerator/endPoint/CreateCommentEndPoints.java
+++ source/comment/src/main/java/bookGenerator/endPoint/CreateCommentEndPoints.java
@@ -13,7 +13,7 @@ import javax.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-
+import bookGenerator._global.event.CommentCreated;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 import bookGenerator.domain.Comment;
@@ -42,23 +42,34 @@ class CreateCommentResDto {
 @RequestMapping("/comments")
 public class CreateCommentEndPoints {
     @PutMapping("/createComment")
-    public ResponseEntity<Void> createComment(@RequestHeader("User-Id") Long userId, @RequestBody CreateCommentReqDto reqDto) {
+    public ResponseEntity<CreateCommentResDto> createComment(@RequestHeader("User-Id") Long userId, @RequestBody CreateCommentReqDto reqDto) {
         try {
 
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
 
             // [1] 새로운 Comment 객체를 CreaterId=userId, bookId, content로 생성
             // [!] createrId, bookId, content만 초기화시키면 되며, 다른 변수들은 자동으로 초기화됨
+            Comment comment = Comment.builder()
+                .createrId(userId)
+                .bookId(reqDto.getBookId())
+                .content(reqDto.getContent())
+                .build();
+            comment = Comment.repository().save(comment);
+            if (comment == null) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            }
             
             // [2] CommentCreated 이벤트를 발생시킴
-
+            CommentCreated commentCreatedEvent = new CommentCreated(comment);
+            commentCreatedEvent.publish();
+            
             // [3] 생성된 Comment 객체의 ID를 반환
-
-            CustomLogger.debug(CustomLoggerType.EXIT);
-
-            return ResponseEntity.status(HttpStatus.OK).build();
+            CreateCommentResDto resDto = new CreateCommentResDto(comment);
+            CustomLogger.debugObject(CustomLoggerType.EXIT, "", resDto);
+            return ResponseEntity.ok(resDto);
 
         } catch(Exception e) {
+            CustomLogger.errorObject(e, "", reqDto);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
