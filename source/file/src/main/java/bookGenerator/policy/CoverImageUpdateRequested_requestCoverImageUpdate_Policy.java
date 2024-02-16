4e076ac06d186c4ebb920240c364add0e8498e5c
wjdwjd <wjdw637@gmail.com>
2024-02-16 14:07:53 +0900

    정책 구현

--- source/file/src/main/java/bookGenerator/policy/CoverImageUpdateRequested_requestCoverImageUpdate_Policy.java
+++ source/file/src/main/java/bookGenerator/policy/CoverImageUpdateRequested_requestCoverImageUpdate_Policy.java
@@ -2,6 +2,7 @@ package bookGenerator.policy;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,13 @@ import org.springframework.stereotype.Service;
 import bookGenerator._global.config.kafka.KafkaProcessor;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
+import bookGenerator.domain.File;
+import bookGenerator.domain.FileManageService;
+import bookGenerator.domain.FileRepository;
+import bookGenerator._global.event.CoverImageGenerationRequestedByFile;
 import bookGenerator._global.event.CoverImageUpdateRequested;
+import bookGenerator._global.event.CoverImageUpdateRequestedByFile;
+import bookGenerator._global.event.ExistingCoverImageDeletingRequested;
 
 @Service
 @Transactional
@@ -29,11 +36,15 @@ public class CoverImageUpdateRequested_requestCoverImageUpdate_Policy {
             CustomLogger.debugObject(CustomLoggerType.ENTER, coverImageUpdateRequested);
 
             // [1] coverImageUpdateRequested.coverImageFileId로 File을 찾음
+            File file = FileManageService.getInstance()
+                    .findByIdOrThrow(coverImageUpdateRequested.getCoverImageFileId());
 
             // [2] File을 이용해서 ExistingCoverImageDeletingRequested 이벤트를 발생시킴
-
+            ExistingCoverImageDeletingRequested deletingRequestedEvent = new ExistingCoverImageDeletingRequested(file);
+            deletingRequestedEvent.publish();
+            
             // [3] File을 이용해서 CoverImageUpdateRequestedByFile 이벤트를 발생시킴
-
+            
             CustomLogger.debug(CustomLoggerType.EXIT);
 
         } catch(Exception e) {
