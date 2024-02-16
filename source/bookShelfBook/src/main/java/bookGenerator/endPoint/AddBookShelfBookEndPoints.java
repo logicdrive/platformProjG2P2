f953c356c25163f1a386011735584119191e7030
wjdwjd <wjdw637@gmail.com>
2024-02-16 11:52:19 +0900

    엔드포인트 구현1

--- source/bookShelfBook/src/main/java/bookGenerator/endPoint/AddBookShelfBookEndPoints.java
+++ source/bookShelfBook/src/main/java/bookGenerator/endPoint/AddBookShelfBookEndPoints.java
@@ -1,5 +1,7 @@
 package bookGenerator.endPoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +14,7 @@ import javax.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-
+import bookGenerator._global.event.BookShelfBookAdded;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 import bookGenerator.domain.BookShelfBook;
@@ -42,21 +44,31 @@ class AddBookShelfBookResDto {
 public class AddBookShelfBookEndPoints {
 
     @PutMapping("/addBookShelfBook")
-    public ResponseEntity<Void> addBookShelfBook(@RequestBody AddBookShelfBookReqDto reqDto) {
+    public ResponseEntity<AddBookShelfBookResDto> addBookShelfBook(@RequestBody AddBookShelfBookReqDto reqDto) {
         try {
 
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
 
             // [1] 새로운 BookShelfBook 객체를 생성
             // [!] bookShelfId, bookId만 초기화시키면 되며, 다른 변수들은 자동으로 초기화됨
+            BookShelfBook bookShel = BookShelfBook.repository().save(
+                BookShelfBook.builder()
+                .bookShelfId(reqDto.getBookShelfId())
+                .bookId(reqDto.getBookId())
+                .build()
+            );
 
             // [2] BookShelfBookAdded 이벤트를 생성된 BookShelfBook로 발생시킴
+            BookShelfBook shelfBook = BookShelfBook.repository().save(bookShel);
+
+            (new BookShelfBookAdded(shelfBook)).publish();
 
             // [3] 생성된 BookShelfBook 객체의 ID를 반환
-                
+            AddBookShelfBookResDto responseDto = new AddBookShelfBookResDto(shelfBook);
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
-            return ResponseEntity.status(HttpStatus.OK).build();
+            return ResponseEntity.ok().body(responseDto);
 
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
--- source/index/src/main/java/bookGenerator/endPoint/EditIndexEndPoints.java
+++ source/index/src/main/java/bookGenerator/endPoint/EditIndexEndPoints.java
@@ -1,5 +1,7 @@
 package bookGenerator.endPoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +14,12 @@ import javax.transaction.Transactional;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
-
+import bookGenerator._global.event.BookDeleted;
+import bookGenerator._global.event.IndexEdited;
 import bookGenerator._global.logger.CustomLogger;
 import bookGenerator._global.logger.CustomLoggerType;
 import bookGenerator.domain.Index;
+import bookGenerator.domain.IndexRepository;
 
 
 @Data
@@ -42,23 +46,34 @@ class EditIndexResDto {
 @RequestMapping("/indexes")
 public class EditIndexEndPoints {
 
+    @Autowired
+    private IndexRepository indexRepository;
+
     @PutMapping("/editIndex")
-    public ResponseEntity<Void> editIndex(@RequestBody EditIndexReqDto reqDto) {
+    public ResponseEntity<EditIndexResDto> editIndex(@RequestBody EditIndexReqDto reqDto) {
         try {
 
             CustomLogger.debugObject(CustomLoggerType.ENTER, reqDto);
 
             // [1] indexId에 해당하는 Index 객체를 찾음
+            Index index = indexRepository.findById(reqDto.getIndexId()).orElse(null);
+            if (index == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
 
             // [2] Index 객체의 name, priority을 변경함
+            index.setName(reqDto.getIndexName());
+            index.setPriority(reqDto.getIndexPriority());
 
             // [3] 저장된 Index를 기반으로 IndexEdited 이벤트를 발생시킴
+            (new IndexEdited(index)).publish();
 
             // [4] 저장된 Index Id를 반환함
-                
+            EditIndexResDto responseDto = new EditIndexResDto(index);
+
             CustomLogger.debug(CustomLoggerType.EXIT);
 
-            return ResponseEntity.status(HttpStatus.OK).build();
+            return ResponseEntity.ok().body(responseDto);
 
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
