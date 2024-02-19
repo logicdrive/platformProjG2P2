f763a06af7046e1be0412037c78012e5b67c7b26
logicdrive <logicdrive7@gmail.com>
2024-02-19 18:05:34 +0900

    Feat: 로깅 내역 추가

--- source/recommendCalcSystem/app/_global/util/HttpUtil.py
+++ source/recommendCalcSystem/app/_global/util/HttpUtil.py
@@ -2,14 +2,20 @@ import requests
 import json
 from dataclasses import dataclass
 
+from ..logger import CustomLogger
+from ..logger import CustomLoggerType
+
 @dataclass
 class HttpMethod:
-    GET = 1
-    PUT = 2
+    GET = "GET"
+    PUT = "PUT"
 
 class HttpUtil:
     @staticmethod
     def jsonRequest(method:HttpMethod, url:str, data=None):
+        CustomLogger.debug(CustomLoggerType.ENTER, "<jsonRequest method: {}, url: {}, dataLength: {}>"\
+                                .format(method, url, len(data) if data else 0))
+
         RES = ""
         if method == HttpMethod.GET:
             RES = requests.get(url)
@@ -21,4 +27,8 @@ class HttpUtil:
         if str(RES.status_code)[0] != "2":
             raise ValueError("Request failed")
 
+
+        CustomLogger.debug(CustomLoggerType.EXIT, "<jsonRequest url: {}, statusCode: {}, responseLength: {}>"\
+                            .format(url, RES.status_code, len(RES.text) if RES.text else 0))
+        
         return json.loads(RES.text) if RES.text else ""
