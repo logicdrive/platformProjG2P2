208c7e847903ec82cf816cfa6b1eec760f9713df
logicdrive <logicdrive7@gmail.com>
2024-02-19 18:49:17 +0900

    Fix: 쿼리에 size가 포함되도록 수정

--- source/recommendCalcSystem/app/_global/proxy/CollectedDataProxy.py
+++ source/recommendCalcSystem/app/_global/proxy/CollectedDataProxy.py
@@ -8,6 +8,8 @@ COLLECTED_DATA_PORT = os.environ.get("COLLECTED_DATA_PORT")
 COLLECTED_DATA_URL = f"http://{COLLECTED_DATA_HOST}:{COLLECTED_DATA_PORT}"
 collectedDataPath = lambda path: f"{COLLECTED_DATA_URL}/{path}"
 
+REQUEST_SIZE = 1000
+
 
 class CollectedDataProxy:
     @staticmethod
@@ -17,35 +19,35 @@ class CollectedDataProxy:
 
     @staticmethod
     def getUsers():
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath("users"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"users?size={REQUEST_SIZE}"))
     
 
     @staticmethod
     def getBooks():
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath("books"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"books?size={REQUEST_SIZE}"))
     
     @staticmethod
     def getBooksByCreaterId(createrId):
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"books/search/findByCreaterId?createrId={createrId}"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"books/search/findByCreaterId?size={REQUEST_SIZE}&createrId={createrId}"))
 
     
     @staticmethod
     def getLikeHistoriesByUserId(userId):
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"likeHistories/search/findByUserId?userId={userId}"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"likeHistories/search/findByUserId?size={REQUEST_SIZE}&userId={userId}"))
 
     
     @staticmethod
     def getBookShelfsByCreaterId(createrId):
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"bookShelfs/search/findByCreaterIdOrderByTitle?createrId={createrId}"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"bookShelfs/search/findByCreaterIdOrderByTitle?size={REQUEST_SIZE}&createrId={createrId}"))
 
     @staticmethod
     def getBookShelfBooksByBookShelfId(bookShelfId):
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"bookShelfBooks/search/findByBookShelfIdOrderByCreatedDateDesc?bookShelfId={bookShelfId}"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"bookShelfBooks/search/findByBookShelfIdOrderByCreatedDateDesc?size={REQUEST_SIZE}&bookShelfId={bookShelfId}"))
 
     
     @staticmethod
     def getTagsByBookId(bookId):
-        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"tags/search/findByBookIdOrderByName?bookId={bookId}"))
+        return HttpUtil.jsonRequest(HttpMethod.GET, collectedDataPath(f"tags/search/findByBookIdOrderByName?size={REQUEST_SIZE}&bookId={bookId}"))
 
 
     @staticmethod
