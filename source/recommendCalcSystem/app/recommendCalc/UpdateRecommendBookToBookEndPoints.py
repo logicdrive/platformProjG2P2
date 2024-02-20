61429fc667059220f48f24b7fd9be08e32402816
logicdrive <logicdrive7@gmail.com>
2024-02-20 11:41:07 +0900

    Feat: 책 관련 책 추천 시스템 구현

--- source/recommendCalcSystem/app/recommendCalc/UpdateRecommendBookToBookEndPoints.py
+++ source/recommendCalcSystem/app/recommendCalc/UpdateRecommendBookToBookEndPoints.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify
 from http import HTTPStatus
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 from .._global.logger import CustomLogger
 from .._global.logger import CustomLoggerType
+from .._global.proxy.CollectedDataProxy import CollectedDataProxy
 
 bp = Blueprint(__name__.split(".")[-1], __name__, url_prefix="/recommendCalc")
 
@@ -35,11 +39,50 @@ def updateRecommendBookToBook() -> UpdateRecommendBookToBookResDto :
         CustomLogger.debug(CustomLoggerType.ENTER)
         
 
+        TOP_N = 3
 
-        resDto = UpdateRecommendBookToBookResDto(1)
+        bookTags, reversedBookTagsIndices = getBookTags()
+
+        countMatrix = CountVectorizer(stop_words="english").fit_transform(getSoups(bookTags))
+        cosineSim2 = cosine_similarity(countMatrix, countMatrix)
+
+        for bookTag in bookTags:
+            recommendBooks = getRecommendBooks(bookTag["bookId"], bookTags, reversedBookTagsIndices, cosineSim2, TOP_N)
+            CollectedDataProxy.recreateRecommendBookToBooks(
+                bookTag["bookId"], 
+                recommendBooks,
+                list(range(1, 1+TOP_N))
+            )
+
+
+        resDto = UpdateRecommendBookToBookResDto(len(bookTags))
         CustomLogger.debugObj(CustomLoggerType.EXIT, resDto)
         return (resDto.json(), HTTPStatus.OK)
 
     except Exception as e :
         CustomLogger.error(e)
         return ("", HTTPStatus.BAD_REQUEST)
+
+
+def getBookTags():
+    bookTags = []
+    for bookId in [book["bookId"] for book in CollectedDataProxy.getBooks()["_embedded"]["books"]] :
+        bookTags.append({
+            "bookId": bookId,
+            "tags": [tag["name"].upper().replace(" ", "") for tag in CollectedDataProxy.getTagsByBookId(bookId)["_embedded"]["tags"]]
+        })
+    
+    reversedBookTagsIndices = {}
+    for i, bookTag in enumerate(bookTags):
+        reversedBookTagsIndices[bookTag["bookId"]] = i
+    
+    return bookTags, reversedBookTagsIndices
+
+def getSoups(bookTags):
+    return [" ".join(bookTag["tags"]) for bookTag in bookTags]
+
+def getRecommendBooks(bookId, bookTags, reversedBookTagsIndices, cosineSim2, topN=3):
+    scores = cosineSim2[reversedBookTagsIndices[bookId]]
+    enumScores = list(enumerate(scores))
+    enumScores = sorted(enumScores, key=lambda x: x[1], reverse=True)
+    return [bookTags[i]["bookId"] for i, _ in enumScores[1:1+topN]]
