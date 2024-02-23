64f58ff7b1b38c09d68a58406f35810be373b528
logicdrive <logicdrive7@gmail.com>
2024-02-23 09:24:45 +0900

    Feat: BookShared UI 기반 구현 작업

--- source/frontend/src/book/sharedList/BookSharedListPage.js
+++ source/frontend/src/book/sharedList/BookSharedListPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Box, Stack, Pagination } from '@mui/material';
 
 import MainNavAppBar from '../../_global/components/MainNavAppBar';
 import BookSubAppBar from '../_global/BookSubAppBar';
+import BookSearchInfo from '../../_global/components/card/BookSearchInfo';
 
 const BookSharedListPage = () => {
     return (
@@ -11,6 +13,25 @@ const BookSharedListPage = () => {
                 searchTypes={[{type: "bookTitle", name: "책 제목"},
                               {type: "bookCreater", name: "책 작성자"}]}
             />
+
+            <Stack>
+                <Stack direction="row" spacing={2}>
+                    <BookSearchInfo
+                        bookId={1}
+                        bookTitle={"점프 투 파이썬"}
+                        bookCreater={"TestCreater"}
+                        bookCreateDate={"2024-02-22 12:47"}
+                        bookLikeCount={10}
+                        bookTags={["AAAAA", "BBBBB", "CCCCC", "DDDDD"]}
+
+                        onClickCardUrl={"/book/info/1"}
+                    />
+                </Stack>
+                
+                <Box sx={{width: "100%", marginTop: "10px", display: "flex", justifyContent: "center"}}>
+                    <Pagination sx={{padding: "auto", margin: "0 auto"}} count={10}/>
+                </Box>
+            </Stack>
         </>
     )
 }
