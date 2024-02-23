42bbe2c69ebbe1b59fbd941c39a94892e4fd13e5
logicdrive <logicdrive7@gmail.com>
2024-02-23 11:14:58 +0900

    Feat: 책장 책 관련 정보 추가

---
+++ source/frontend/src/bookShelf/info/BookShelfBookSearchInfo.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { Card, CardContent, Box, IconButton, Stack } from '@mui/material';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import BoldText from '../../_global/components/text/BoldText';
+
+const BookShelfBookSearchInfo = ({bookId, bookImageUrl, bookTitle, bookCreater, bookCreateDate, bookLikeCount, bookTags, onClickCardUrl}) => {
+    const navigate = useNavigate()
+
+    return (
+        <Card sx={{width: "380px", height: "220px"}} onClick={()=>{navigate(onClickCardUrl)}}>
+            <CardContent sx={{padding: "10px"}}>
+                <Box sx={{float: "left", cursor: "pointer"}}>
+                    <Box
+                        component="img"
+                        sx={{
+                            height: 200,
+                            width: 110,
+                            backgroundColor: "lightgray",
+                            borderRadius: 3,
+                            border: "1px solid lightgray",
+
+                        }}
+                        alt="업로드된 이미지가 표시됩니다."
+                        src={(bookImageUrl) ? bookImageUrl : "/src/NoImage.jpg"}
+                    />
+                </Box>
+                <Stack sx={{float: "left", marginLeft: "10px", width: "238px"}}>
+                    <Box>
+                        <BoldText sx={{float: "left", fontSize: "18px", cursor: "pointer"}}>{bookTitle}</BoldText>
+                        <BoldText sx={{float: "right", fontSize: "18px", cursor: "pointer", "&:hover": {opacity: 0.80}}} onClick={(e)=>{e.stopPropagation(); alert("Deleted")}}>
+                            <DeleteIcon sx={{color: "gray"}}/>
+                        </BoldText>
+                    </Box>
+
+                    <BoldText sx={{fontSize: "15px", color:"lightgray", cursor: "pointer"}}>작성자: {bookCreater}</BoldText>
+                    <BoldText sx={{fontSize: "15px", color:"lightgray", cursor: "pointer"}}>작성일: {bookCreateDate}</BoldText>
+                    
+                    <IconButton onClick={(e)=>{e.stopPropagation(); alert("BBB")}} sx={{paddingY: "0px", borderRadius: "5px", marginLeft: "-10px", width: "110px"}}>
+                        <ThumbUpIcon sx={{fontSize: "20px"}}/> 
+                        <BoldText sx={{marginTop: "3px", marginLeft: "3px", fontSize: "15px", color: "gray"}}>{bookLikeCount} LIKES</BoldText>
+                    </IconButton>
+
+                    <Box sx={{display: "flex", flexDirection: "row", width: "215px", flexWrap: "wrap-reverse", marginTop: "75px", marginLeft: "-3px"}}>
+                        {
+                            bookTags.map((tag, index) => {
+                                return (
+                                    <BoldText key={index} sx={{fontSize: "10px", backgroundColor: "lightgray", padding: "5px", display: "inline-block", color: "gray", borderRadius: "5px", margin: "2px", cursor: "context-menu"}}>{tag}</BoldText>
+                                )
+                            })
+                        }
+                    </Box>
+                </Stack>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default BookShelfBookSearchInfo;
--- source/frontend/src/bookShelf/info/BookShelfInfoPage.js
+++ source/frontend/src/bookShelf/info/BookShelfInfoPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Box, Stack, Pagination } from '@mui/material';
 
 import MainNavAppBar from '../../_global/components/MainNavAppBar';
 import BookShelfInfoSubAppBar from './BookShelfInfoSubAppBar';
+import BookShelfBookSearchInfo from './BookShelfBookSearchInfo';
 
 const BookShelfInfoPage = () => {
     const {bookShelfId} = useParams()
@@ -17,6 +19,26 @@ const BookShelfInfoPage = () => {
                     {type: "bookCreater", name: "책 작성자"}
                 ]}
             />
+
+            <Stack>
+                <Stack direction="row" spacing={2}>
+                    <BookShelfBookSearchInfo
+                        bookId={1}
+                        bookTitle={"점프 투 파이썬"}
+                        bookCreater={"TestCreater"}
+                        bookCreateDate={"2024-02-22 12:47"}
+                        bookLikeCount={10}
+                        bookTags={["AAAAA", "BBBBB", "CCCCC", "DDDDD"]}
+
+                        onClickCardUrl={"/book/info/1"}
+                        bookImageUrl={""}
+                    />
+                </Stack>
+                
+                <Box sx={{width: "100%", marginTop: "10px", display: "flex", justifyContent: "center"}}>
+                    <Pagination sx={{padding: "auto", margin: "0 auto"}} count={10}/>
+                </Box>
+            </Stack>
         </>
     )
 }
