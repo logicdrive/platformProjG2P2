a345d52b3295f4725f3e7858d67a6b16b2916c94
logicdrive <logicdrive7@gmail.com>
2024-02-23 15:22:49 +0900

    Feat: 책 표지 이미지 기반 UI 추가

--- source/frontend/src/book/manage/BookManagePage.js
+++ source/frontend/src/book/manage/BookManagePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Box, IconButton, Divider, Container } from "@mui/material";
+import { Box, IconButton, Divider, Container, Stack } from "@mui/material";
 import { useParams, useNavigate } from 'react-router-dom';
 import DoneIcon from '@mui/icons-material/Done';
 import EditIcon from '@mui/icons-material/Edit';
+import UploadIcon from '@mui/icons-material/Upload';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import ImageIcon from '@mui/icons-material/Image';
 
 import MainNavAppBar from '../../_global/components/MainNavAppBar';
 import BoldText from '../../_global/components/text/BoldText';
+import NavText from '../../_global/components/text/NavText';
 
 const BookManagePage = () => {
     const navigate = useNavigate()
@@ -16,7 +20,7 @@ const BookManagePage = () => {
         <>
             <MainNavAppBar focusedIndex={0} backArrowUrl="/book/myList"/>
 
-            <Container maxWidth="md" sx={{padding: "5px", marginTop: "10px"}}>
+            <Container maxWidth="md" sx={{marginTop: "15px"}}>
                 <Box sx={{width: "100%", height: "30px", marginTop: "5px"}}>
                     <EditIcon sx={{float: "left", marginTop: "5px"}}/>
                     <BoldText sx={{float: "left", fontSize: "20px", marginTop: "5px", marginLeft: "5px"}}>책 편집</BoldText>
@@ -25,6 +29,48 @@ const BookManagePage = () => {
                         <DoneIcon sx={{fontSize: "30px", color: "black"}}/>
                         <BoldText sx={{float: "left", fontSize: "20px", marginTop: "5px"}}>완료</BoldText>
                     </IconButton>
+                </Box>
+                <Divider sx={{width: "100%"}}/>
+
+                <Box sx={{display: "flex"}}>
+                    <Stack sx={{float: "left", width: "220px", display: "flex", justifyContent: "center"}}>
+                        <Box sx={{paddingLeft: "7px", paddingTop: "5px"}}>
+                            <ImageIcon sx={{float: "left", color: "black"}}/>
+                            <NavText sx={{float: "left", marginTop: "2px", marginLeft: "5px", color: "black"}}>책 표지 이미지</NavText>
+                        </Box>
+
+                        <Box
+                            component="img"
+                            sx={{
+                                height: 300,
+                                width: 200,
+                                backgroundColor: "lightgray",
+                                borderRadius: 3,
+                                border: "1px solid lightgray",
+                                marginX: "auto",
+                                marginTop: "5px"
+                            }}
+                            alt="업로드된 이미지가 표시됩니다."
+                            src={"/src/NoImage.jpg"}
+                        />
+                        
+                        <Box sx={{marginTop: "5px", marginLeft: "18px", marginBottom: "5px"}}>
+                            <Box onClick={()=>{alert("Gen")}} sx={{float: "left", backgroundColor: "cornflowerblue", width: "63px", height: "25px", padding: "8px", borderRadius: "5px", cursor: "pointer", "&:hover": {opacity: 0.80}}}>
+                                <SmartToyIcon sx={{float: "left", color: "white"}}/>
+                                <NavText sx={{float: "left", marginTop: "2px", marginLeft: "5px"}}>생성</NavText>
+                            </Box>
+
+                            <Box onClick={()=>{alert("Upload")}} sx={{marginLeft: "5px", float: "left", backgroundColor: "cornflowerblue", width: "82px", height: "25px", padding: "8px", borderRadius: "5px", cursor: "pointer", "&:hover": {opacity: 0.80}}}>
+                                <UploadIcon sx={{float: "left", color: "white"}}/>
+                                <NavText sx={{float: "left", marginTop: "2px", marginLeft: "5px"}}>업로드</NavText>
+                            </Box>
+                        </Box>
+                    </Stack>
+
+                    <Divider orientation="vertical" flexItem/>
+                    <Stack sx={{float: "left", flex: "1 0 auto", paddingX: "5px"}}>
+
+                    </Stack>
                 </Box>
                 <Divider sx={{width: "100%"}}/>
             </Container>
