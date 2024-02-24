4a8dae0240cf384557be4d18ccd80a6ea1e97b3a
logicdrive <logicdrive7@gmail.com>
2024-02-24 12:41:03 +0900

    Fix: 위치 스타일 수정

--- source/frontend/src/bookShelf/_global/BookShelfSearchInfo.js
+++ source/frontend/src/bookShelf/_global/BookShelfSearchInfo.js
@@ -136,7 +136,7 @@ const BookShelfSearchInfo = ({rawBookShelfInfo, isEditIconVisible}) => {
                         <BoldText sx={{float: "left", fontSize: "18px", cursor: "pointer"}}>{bookShelfInfo.title}</BoldText>
                         {
                             (isEditIconVisible) ? (
-                                <BoldText sx={{float: "right", fontSize: "18px", cursor: "pointer", "&:hover": {opacity: 0.80}, position: "relative", left: "3px"}} onClick={(e)=>{e.stopPropagation();}}>
+                                <BoldText sx={{float: "right", fontSize: "18px", cursor: "pointer", "&:hover": {opacity: 0.80}, position: "relative", left: "1px"}} onClick={(e)=>{e.stopPropagation();}}>
                                     {
                                         (bookShelfInfo.isShared) ? (
                                             <YesNoButton onClickYes={()=>{onClickSharedButton(false)}} title="해당 책장의 공유를 취소하시겠습니까?">
