7b83cb03cf8a76bcd2ddd536ebbd11c1383b1d76
logicdrive <logicdrive7@gmail.com>
2024-02-26 18:37:05 +0900

    Feat: 서비스 생성 스크립트 구현

--- command/kubernates/services/deleteAll.ps1
+++ command/kubernates/services/deleteAll.ps1
@@ -1,1 +1,20 @@
 Start-Process powershell -ArgumentList "-Command", "cd ./source/user;./command/kubernetes/deleteAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/book;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/tag;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/index;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/content;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/problem;./command/kubernetes/deleteAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/comment;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/bookShelf;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/bookShelfBook;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/file;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/collectedData;./command/kubernetes/deleteAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/externalSystemProxy;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/externalSystem;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/recommendCalcSystem;./command/kubernetes/deleteAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/gateway;./command/kubernetes/deleteAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/frontend;./command/kubernetes/deleteAll.ps1"
--- command/kubernates/services/runAll.ps1
+++ command/kubernates/services/runAll.ps1
@@ -1,1 +1,20 @@
 Start-Process powershell -ArgumentList "-Command", "cd ./source/user;./command/kubernetes/makeAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/book;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/tag;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/index;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/content;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/problem;./command/kubernetes/makeAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/comment;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/bookShelf;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/bookShelfBook;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/file;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/collectedData;./command/kubernetes/makeAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/externalSystemProxy;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/externalSystem;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/recommendCalcSystem;./command/kubernetes/makeAll.ps1"
+
+Start-Process powershell -ArgumentList "-Command", "cd ./source/gateway;./command/kubernetes/makeAll.ps1"
+Start-Process powershell -ArgumentList "-Command", "cd ./source/frontend;./command/kubernetes/makeAll.ps1"
