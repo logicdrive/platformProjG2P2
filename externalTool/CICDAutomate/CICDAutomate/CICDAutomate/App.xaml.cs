4383f05a6102de3813082cae97530bd2bbf803ce
logicdrive <logicdrive7@gmail.com>
2024-03-02 17:44:56 +0900

    Feat: 실행 기반 구현

---
+++ externalTool/CICDAutomate/CICDAutomate/CICDAutomate/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CICDAutomate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
