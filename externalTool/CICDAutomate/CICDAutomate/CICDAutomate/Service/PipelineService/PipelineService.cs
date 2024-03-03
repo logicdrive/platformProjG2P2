2a755c839106895df981522b52c487c6adc27d7c
logicdrive <logicdrive7@gmail.com>
2024-03-04 08:56:43 +0900

    Feat: Pipeline 서비스 기반 마련

---
+++ externalTool/CICDAutomate/CICDAutomate/CICDAutomate/Service/PipelineService/PipelineService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CICDAutomate.Service.PipelineService
+{
+    class PipelineService
+    {
+        public List<PipelineDto> pipelineDtoList { get; set; } = new List<PipelineDto>();
+
+        
+    }
+
+
+    class PipelineDto
+    {
+        public string title { get; set; } = "";
+        public DateTime createdDate { get; set; } = DateTime.Now;
+        public DateTime updatedDate { get; set; } = DateTime.Now;
+
+        List<ActionDto> actionList { get; set; } = new List<ActionDto>();
+
+
+        public string dataPath { get; set; } = "";
+        public string logPath { get; set; } = "";
+    }
+
+    class ActionDto
+    {
+        public string selectImagePath { get; set; } = "";
+        public string title { get; set; } = "";
+        public string description { get; set; } = "";
+        Window editWindow { get; set; } = null;
+    }
+}
---
+++ externalTool/CICDAutomate/CICDAutomate/CICDAutomate/Util/HttpUtil.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+
+namespace ServerTester.Util
+{
+    // Make a HTTP reuqest to the server
+    internal class HttpUtil
+    {
+        // Make request request by using json
+        public static RestResponse request(Method method, string baseUrl, string resourceUrl, string jsonString="", Dictionary<string, string> headers=null)
+        {
+            RestRequest request = new RestRequest(resourceUrl, method);
+            if (jsonString != "")
+                request.AddJsonBody(jsonString);
+            if (headers != null)
+                foreach (var header in headers)
+                    request.AddHeader(header.Key, header.Value);
+
+            return (new RestClient(baseUrl)).Execute(request);
+        }
+
+        public static Method getMethodByString(string method)
+        {
+            switch (method.ToUpper())
+            {
+                case "GET":
+                    return Method.Get;
+                case "POST":
+                    return Method.Post;
+                case "PUT":
+                    return Method.Put;
+                case "DELETE":
+                    return Method.Delete;
+                default:
+                    return Method.Get;
+            }
+        }
+    }
+}
